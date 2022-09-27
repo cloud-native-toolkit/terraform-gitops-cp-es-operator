254d489b35021112ea0a93ffcc179cfa5591457b
Bala Sivasubramanian <bala@us.ibm.com>
2022-09-27 13:33:53 -0500

    update gitops provider (#14)
    * update gitops provider
    
    Signed-off-by: balasgit <bala@us.ibm.com>
    
    * Update variables.tf
    
    * Update main.tf
    
    * Update Gitops Provider
    
    * Update Gitops Provider
    
    Signed-off-by: balasgit <bala@us.ibm.com>
    Co-authored-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,16 +1,25 @@
+
+
 terraform {
-}
-module "setup_clis" {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
-  clis   = ["yq", "jq"]
-  # clis   = [“helm”, “rosa”]
-}
-resource "null_resource" "setup_clis" {
-  depends_on = [
-    "module.setup_clis"
-  ]
-  provisioner "local-exec" {
-    #command = “echo -n ‘${module.clis.bin_dir}’ > .bin_dir”
-    command = "echo -n '${module.setup_clis.bin_dir}' > .bin_dir"
+  required_providers {
+    gitops = {
+     source = "cloud-native-toolkit/gitops"
+     }
   }
+}
+
+
+module setup_test_clis {
+    source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+
+    bin_dir = "${path.cwd}/test_bin_dir"
+    clis = ["kubectl", "oc"]
+}
+
+
+resource local_file bin_dir {
+    filename = "${path.cwd}/.bin_dir"
+
+
+    content = module.setup_test_clis.bin_dir
 }
